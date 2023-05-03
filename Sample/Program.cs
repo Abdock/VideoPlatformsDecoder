a6093e056ef194bc4032e115718951e0b3b6f175
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-05-03 17:08:20 +0600

    Sample for YouTube parser added

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,3 +1,4 @@
 ﻿using Sample;
 
 await TikTokSample.RunAsync();
+await YouTubeSample.RunAsync();
---
+++ Sample/YouTubeSample.cs
@@ -0,0 +1,17 @@
+﻿using VideoPlatform.YouTube;
+
+namespace Sample;
+
+public static class YouTubeSample
+{
+    public static async Task RunAsync()
+    {
+        using var client = new HttpClient();
+        var youtube = new YouTubeVideoService(client);
+        var url = new Uri("https://www.youtube.com/watch?v=gquRl13WryU");
+        var sourceLink = await youtube.DecodeUrlAsync(url);
+        using var request = new HttpRequestMessage(HttpMethod.Head, sourceLink);
+        var response = await client.SendAsync(request);
+        Console.WriteLine($"YouTube: {response.StatusCode}");
+    }
+}
