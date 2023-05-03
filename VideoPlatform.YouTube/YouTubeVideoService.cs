ca45d977269dbe5ccad1e5cb59d5d5b1565565fa
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-05-03 16:41:33 +0600

    Add stub for YouTube

---
+++ VideoPlatform.YouTube/YouTubeVideoService.cs
@@ -0,0 +1,16 @@
+﻿namespace VideoPlatform.YouTube;
+
+public class YouTubeVideoService : IVideoService
+{
+    public string ServiceBaseUrl => "https://www.youtube.com/";
+
+    public async Task<Uri> DecodeUrlAsync(Uri url, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> RefreshCookiesAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
