a6ff5a20c6eebd5f8a840e13fed639967f33eb7a
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-05-04 18:29:42 +0600

    Move http client initialization to separate method

--- VideoPlatform.Test/TikTokTests.cs
+++ VideoPlatform.Test/TikTokTests.cs
@@ -5,6 +5,14 @@ namespace VideoPlatform.Test;
 
 public class TikTokTests
 {
+    private HttpClient _client = null!;
+    
+    [OneTimeSetUp]
+    public void Initialize()
+    {
+        _client = new HttpClient();
+    }
+    
     [TestCase("https://vm.tiktok.com/ZMYKLCMJm/")]
     [TestCase("https://www.tiktok.com/@106sm/video/7224117086312860933?is_from_webapp=1&sender_device=pc&web_id=7207752210184209926")]
     [TestCase("https://www.tiktok.com/@106sm/video/7224117086312860933")]
@@ -12,15 +20,14 @@ public class TikTokTests
     {
         //Arrange
         const HttpStatusCode expectedResult = HttpStatusCode.OK;
-        using var client = new HttpClient();
-        IVideoService tiktok = new TikTokVideoService(client);
+        IVideoService tiktok = new TikTokVideoService(_client);
         var sourceLink = await tiktok.DecodeUrlAsync(new Uri(link));
         var cookies = await tiktok.RefreshCookiesAsync();
         using var request = new HttpRequestMessage(HttpMethod.Head, sourceLink);
         request.Headers.Add("Cookie", cookies);
         request.Headers.Add("Referer", tiktok.ServiceBaseUrl);
         //Act
-        var response = await client.SendAsync(request);
+        var response = await _client.SendAsync(request);
         //Assert
         if (response.StatusCode == HttpStatusCode.Forbidden)
         {
