c32fc5d7eb600d6fb2461ed12134c7b6b66a0b32
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-05-04 18:24:02 +0600

    Test for TikTok added

---
+++ VideoPlatform.Test/TikTokTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using VideoPlatform.TikTok;
+
+namespace VideoPlatform.Test;
+
+public class TikTokTests
+{
+    [TestCase("https://vm.tiktok.com/ZMYKLCMJm/")]
+    [TestCase("https://www.tiktok.com/@106sm/video/7224117086312860933?is_from_webapp=1&sender_device=pc&web_id=7207752210184209926")]
+    [TestCase("https://www.tiktok.com/@106sm/video/7224117086312860933")]
+    public async Task DecodeUrlAsync_TryDecodeCorrectTikTokUrl_HeadRequestReturnsOk(string link)
+    {
+        //Arrange
+        const HttpStatusCode expectedResult = HttpStatusCode.OK;
+        using var client = new HttpClient();
+        IVideoService tiktok = new TikTokVideoService(client);
+        var sourceLink = await tiktok.DecodeUrlAsync(new Uri(link));
+        var cookies = await tiktok.RefreshCookiesAsync();
+        using var request = new HttpRequestMessage(HttpMethod.Head, sourceLink);
+        request.Headers.Add("Cookie", cookies);
+        request.Headers.Add("Referer", tiktok.ServiceBaseUrl);
+        //Act
+        var response = await client.SendAsync(request);
+        //Assert
+        if (response.StatusCode == HttpStatusCode.Forbidden)
+        {
+            await TestContext.Out.WriteLineAsync(sourceLink.ToString());
+        }
+        
+        Assert.That(response.StatusCode, Is.EqualTo(expectedResult));
+    }
+}
