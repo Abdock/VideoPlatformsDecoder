74d033c7db4721365570bab16630a1e339ada19c
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-05-05 23:43:02 +0600

    Added new tests

--- VideoPlatform.Test/YouTubeTests.cs
+++ VideoPlatform.Test/YouTubeTests.cs
@@ -14,8 +14,16 @@ public class YouTubeTests
         _client = new HttpClient();
     }
 
+    [TestCase("https://youtu.be/CL8ihD3rPC4")]
     [TestCase("https://www.youtube.com/shorts/6tX2xK23Hk4")]
     [TestCase("https://www.youtube.com/shorts/9xJFuDi7Ir8")]
+    [TestCase("https://www.youtube.com/watch?v=LDhPs8BAFKk")]
+    [TestCase("https://www.youtube.com/shorts/SqwM04cHNqY")]
+    [TestCase("https://www.youtube.com/shorts/2ydko8o8SUE")]
+    [TestCase("https://www.youtube.com/shorts/9g6Bh-tMPg8")]
+    [TestCase("https://www.youtube.com/shorts/zKX5S_uw8AQ")]
+    [TestCase("https://www.youtube.com/shorts/LfFLexL3ckE")]
+    [TestCase("https://youtube.com/shorts/73xblB0i05U?feature=share")]
     [TestCase("https://www.youtube.com/watch?v=5EKquLnbo0k&list=RDMM5EKquLnbo0k&start_radio=1")]
     public async Task DecodeUrlAsync_TryDecodeCorrectYouTubeLinks_HeadRequestsReturnsOk(string link)
     {
@@ -28,11 +36,6 @@ public class YouTubeTests
         //Act
         var response = await _client.SendAsync(request);
         //Assert
-        if (response.StatusCode == HttpStatusCode.Forbidden)
-        {
-            await TestContext.Out.WriteLineAsync(string.Join("\n", logger.Logs));
-        }
-        
         Assert.That(response.StatusCode, Is.EqualTo(expectedResult));
     }
 
